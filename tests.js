220d63c13e412943ad6352bd68d8e9b8bbf8e5d4
GabyBarajitas <gabybarajasbcn@gmail.com>
2022-05-26 10:02:09 +0200

    Objetivo COMPLETADO!!!! Bravo para mi!!!

--- tests.js
+++ tests.js
@@ -418,9 +418,8 @@ describe("Operaciones iterables", () => {
         let list = [1,5,7,9,11,13];
 
         // utiliza la función map para aplicar los números de la lista
-        list = "???";
+        list = list.map(n => n-1);
         
-
         expect(list).to.have.same.members([0,4,6,8,10,12]);
     })
 
@@ -429,7 +428,8 @@ describe("Operaciones iterables", () => {
         let list = ["Pikachu", "Charmander", "Magikarp"];
 
         // utiliza la función map para aplicar los números de la lista y guardar la nueva lista en result
-                
+        let result = list.map(n => n + ", te elijo a ti!");
+
         expect(result).to.have.same.members([
             "Pikachu, te elijo a ti!",
             "Charmander, te elijo a ti!",
@@ -442,7 +442,7 @@ describe("Operaciones iterables", () => {
         let knownExoplanets = [ "TOI-1298 b","TOI-132 b","TOI-1333 b","TOI-1338 b","TOI-1431 b","TOI-1444 b","TOI-1478 b","TOI-150.01","TOI-157 b","TOI-1601 b","TOI-163 b","TOI-1634 b","TOI-1685 b","TOI-169 b","TOI-172 b","TOI-1728 b","TOI-1749 b","TOI-1749 c"];
 
         // utiliza la función includes para comprobar si la lista de planetas contiene el planeta TOI-1634 b
-        let result = "???";
+        let result = knownExoplanets.includes("TOI-1634 b");
         
         expect(result).to.equal(true);        
     })
@@ -452,12 +452,13 @@ describe("Operaciones iterables", () => {
         let knownExoplanets = [ "TOI-1298 b","TOI-132 b","TOI-1333 b","TOI-1338 b","TOI-1431 b","TOI-1444 b","TOI-1478 b","TOI-150.01","TOI-157 b","TOI-1601 b","TOI-163 b","TOI-1634 b","TOI-1685 b","TOI-169 b","TOI-172 b","TOI-1728 b","TOI-1749 b","TOI-1749 c"];
 
         // utiliza la función filter para quedarnos con los planetas que terminan por la letra c
-        // puedes utilizar la función .endsWith para comprobar si un string termina por una letra
+        // puedes utilizar la función .endsW(ith para comprobar si un string termina por una letra
         // p.ej. "Hola".endsWith('a') devuelve true
-        let result = "???";
+        let result = knownExoplanets.filter(n => n.endsWith("c"));
         
 
         // todos los planetas de result terminan con la letra c
+
         expect(result.every( p => p.endsWith('c'))).to.equal(true);        
     })
 
